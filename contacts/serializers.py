8c16dd7da7e73f89088d3e1b082f33b5ce04aa81
Kolawole Daniel <kolawolecoke@gmail.com>
2022-12-31 11:51:35 +0100

    contacts api created

---
+++ contacts/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Contact
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = ['country_code',
+                  'first_name',
+                  'last_name',
+                  'phone_number',
+                  'contact_picture',
+                  'is_favorite']
