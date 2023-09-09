d01917677c5e2ede16c6eb85e80728d6f29e3890
Kolawole Daniel <kolawolecoke@gmail.com>
2023-09-09 20:59:07 +0100

    TESTING: created test cases for creating contacts

--- contacts/tests.py
+++ contacts/tests.py
@@ -4,8 +4,9 @@ from django.conf import settings
 from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
 from rest_framework import status
-from .views import ContactListCreateView, ContactDetailView
 import jwt
+from .views import ContactListCreateView, ContactDetailView
+from .serializers import ContactSerializer
 
 
 # Create your tests here.
@@ -27,6 +28,7 @@ class TestContactListCreateView(APITestCase):
     contacts_url = reverse("contacts")
 
     def setUp(self):
+        self.serializer_class = ContactSerializer
         user_details = {
             "username": "testuser",
             "email": "testuser@gmail.com",
@@ -41,11 +43,34 @@ class TestContactListCreateView(APITestCase):
 
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
 
-    def test_get_contacts_authenticated(self):
+    def test_list_contacts_authenticated(self):
+        serializer = self.serializer_class()
         response = self.client.get(self.contacts_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # self.assertEqual(
+        #     response.data,
+        # )
 
-    def test_get_contacts_unauthenticated(self):
+    def test_list_contacts_unauthenticated(self):
         self.client.force_authenticate(user=None, token=None)
         response = self.client.get(self.contacts_url)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_contact_authenticated(self):
+        data = {
+            "owner": self.user,
+            "country_code": "+234",
+            "first_name": "test",
+            "last_name": "contact1",
+            "phone_number": "8736767278",
+        }
+        serializer = self.serializer_class(data=data)
+
+        self.assertEqual(serializer.is_valid(), True)
+        response = self.client.post(self.contacts_url, serializer.data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["first_name"], data["first_name"])
+
+
+class TestCustomerDetailView(APITestCase):
+    pass
